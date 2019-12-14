ef9463ddd9b7550ece30dddebc7092281bc5f4b6
Hexzhe <hexzhe@jsngbt.com>
2019-12-14 00:04:34 -0500

    Replaced the default Camion name to an actual truck model...

--- 8PRO128-TP4/Camion.h
+++ 8PRO128-TP4/Camion.h
@@ -4,7 +4,7 @@
 class Camion
 {
 public:
-	Camion(std::string marque = "Honda", std::string modele = "Civic", int annee = 2020, std::string moteur = "WD-40");
+	Camion(std::string marque = "Ford", std::string modele = "F-150", int annee = 2020, std::string moteur = "WD-40");
 	~Camion();
 
 	std::string getModeleMoteur();
